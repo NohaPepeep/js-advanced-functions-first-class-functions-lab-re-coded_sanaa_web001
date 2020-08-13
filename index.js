2ac98ad5b80214b2b2f8789606b8e275a1ded25e
Nuha Al-Sameai <noha.pepeep1@gmail.com>
2020-08-13 01:32:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers=function(drivers){
+  drivers=[drivers[0],drivers[1]]
+  return drivers
+}
+const returnLastTwoDrivers=function(drivers){
+  drivers=[drivers[drivers.length-2],drivers[drivers.length-1]]
+  return drivers
+}
+let drivers=["4","5","6"]
+const selectingDrivers=[returnFirstTwoDrivers(drivers),returnLastTwoDrivers(drivers)];
